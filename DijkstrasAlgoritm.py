5f0f24460d77111f2fa47c2797e41bd70d8411c4
winkelh <winkelh@gmail.com>
2026-09-08 09:13:31 +0200

    save

--- DijkstrasAlgoritm.py
+++ DijkstrasAlgoritm.py
@@ -5,9 +5,10 @@ from typing import KeysView
 class Graph:
     def __init__(self, graph: Dict = None) -> None:
         self.graph = graph if graph is not None else {}
+        self.nodes = {}
 
     def add_node(self, name: str) -> None:
-        self.graph[name] = {"visited": False, "neighbors": {}}
+        self.graph[name] = {"neighbors": {}}
 
     def add_edge(self, start_node: str, end_node: str, weigth: int) -> None:
         edge_id = start_node + "_" + end_node
@@ -24,27 +25,28 @@ class Graph:
         return self.graph
         
     def find_shortest_route(self, start_node: str, end_node: str) -> Dict:
-        # initialize the nodes with distance and visited status
-        unvisited_nodes = set(self.graph.keys())
-        nodes = {}
-        for node in unvisited_nodes:
-            nodes[node] = {
-                "distance": float('inf'),
-                "visited": False,
-                "previous": None
+        # initialize the nodes with distance_to_start and visited status
+        for node in self.graph.keys():
+            self.nodes[node] = {
+                "distance_to_start": float('inf'),
+                "visited" : False,
             }
-        nodes[start_node]["distance"] = 0
+        self.nodes[start_node]["distance_to_start"] = 0
 
-        # Walk through the nodes
-        current_node = self.graph[start_node]
-        self.graph[start_node]["visited"] = True
-        for neighbor in self.graph[start_node]["neighbors"]:
-            distance = self.graph[start_node]["neighbors"][neighbor]
-            self.graph[neighbor]["visited"] = True
-            if distance < self.graph[start_node]["neighbors"][neighbor]: 
-                self.graph[start_node]["neighbors"][neighbor] = distance
-            print(f"neighbor: {neighbor}; distance: {distance}")
-        return nodes
+        # Walk through the nodes, start with the start node and walk through its unvisited neighbors.
+        current_node = start_node
+        for neighbor in self.graph[current_node]["neighbors"]:
+            current_node_distance_to_start = self.nodes[current_node]["distance_to_start"]
+            edge_weight                    = self.graph[start_node]["neighbors"][neighbor]
+            neighbor_distance_to_start     = self.nodes[neighbor]["distance_to_start"]
+            if current_node_distance_to_start + edge_weight < neighbor_distance_to_start: 
+                self.nodes[neighbor]["distance_to_start"] = current_node_distance_to_start + edge_weight
+            print(f"Debug: Start Node: {current_node} current_node_distance_to_start: {current_node_distance_to_start}; Neighbor node: {neighbor} Edge weigth: {edge_weight} / distance to start: {self.nodes[neighbor]["distance_to_start"]}")
+            
+            self.graph[current_node]["visited"] = True
+            # Now initialize the next current node
+
+        return self.nodes
         
 
 ed = Graph()
@@ -52,6 +54,7 @@ ed.add_node("A")
 ed.add_node("B")
 ed.add_node("C")
 ed.add_node("D")
+print(f"List of the nodes: {ed.get_nodes()}")
 
 ed.add_edge("A", "B", 10)
 ed.add_edge("A", "C", 15)
